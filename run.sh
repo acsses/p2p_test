3885352eaca84a8de7c3b4e0a6d19d58d2a63bdc
Eagle Code <tomoki.takaba@outlook.jp>
2023-10-29 18:35:03 +0900

    no message

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-gcc request.c p2ptest/http.c p2ptest/parser.c p2ptest/ssdp.c p2ptest/timeutil.c p2ptest/natpmp.c p2ptest/portmapping.c p2ptest/network.c p2ptest/id.c p2ptest/sha256.c -L/usr/local/opt/expat/lib -I/usr/local/opt/expat/include -lexpat -L/usr/local/opt/json-c/lib -I/usr/local/opt/json-c/include -ljson-c -L/usr/local/opt/openssl@3.1/lib -I/usr/local/opt/openssl@3.1/include -lssl -lcrypto -o request
+gcc request.c p2ptest/http.c p2ptest/parser.c p2ptest/ssdp.c p2ptest/timeutil.c p2ptest/natpmp.c p2ptest/portmapping.c p2ptest/network.c p2ptest/id.c p2ptest/sha256.c -L/usr/local/opt/expat/lib -I/usr/local/opt/expat/include -lexpat -L/usr/local/opt/json-c/lib -I/usr/local/opt/json-c/include -ljson-c -L/usr/local/opt/openssl@3.1/lib -I/usr/local/opt/openssl@3.1/include -lssl -lcrypto -o request -W
 ./request
 
 ##gcc test2.c p2ptest/http.c p2ptest/parser.c p2ptest/ssdp.c p2ptest/timeutil.c p2ptest/natpmp.c p2ptest/portmapping.c p2ptest/network.c -L/usr/local/opt/expat/lib -I/usr/local/opt/expat/include -lexpat -L/usr/local/opt/json-c/lib -I/usr/local/opt/json-c/include -ljson-c -L/usr/local/opt/openssl@3.1/lib -I/usr/local/opt/openssl@3.1/include -lssl -lcrypto

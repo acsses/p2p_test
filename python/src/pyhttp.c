8df89e14b748dd1586e0a47a233c4256bd0160c3
Eagle Code <tomoki.takaba@outlook.jp>
2023-11-02 17:21:52 +0900

    [fix]
    request.c
    python/src/pyhttp.c
    
    fix the python library filles

--- python/src/pyhttp.c
+++ python/src/pyhttp.c
@@ -3,7 +3,7 @@
 
 extern int requestHttpGET(char buff[], int buf_size,char * URL,int is_local);
 
-//extern int requestHttpsGET(char buff[], int buf_size,char * URL);
+extern int requestHttpsGET(char buff[], int buf_size,char * URL);
 
 //extern int requestHttpPOST(char buff[], int buf_size,char * URL,int is_local,char header[],char body[]);
 //
@@ -13,49 +13,53 @@ extern int requestHttpGET(char buff[], int buf_size,char * URL,int is_local);
 static PyObject* reqGET(PyObject* self, PyObject* args){
     char buff[2048];
 
-    char url[256];
+    char* url;
     int is_local;
+    
 
-    if (!PyArg_ParseTuple(args, "si", &url, &is_local)){
+    if (!PyArg_ParseTuple(args, "si", &url,&is_local)){
         return NULL;
     }
 
     requestHttpGET(buff,2048,url,is_local);
-    return Py_BuildValue("s", buff);
+
+    return Py_BuildValue("y", buff);
 };
 
-//static PyObject* reqGETs(PyObject* self, PyObject* args){
-//    char buff[2048];
-//
-//    char url[256];
-//    int is_local;
-//
-//    if (!PyArg_ParseTuple(args, "s", &url)){
-//        return NULL;
-//    }
-//
-//    requestHttpsGET(buff,2048,url,is_local);
-//    return Py_BuildValue("s", buff);
-//};
-//
+static PyObject* reqGETs(PyObject* self, PyObject* args){
+    char buff[2048];
+
+    char* url;
+    int is_local;
+
+    if (!PyArg_ParseTuple(args, "s", &url)){
+        return NULL;
+    }
+
+    requestHttpsGET(buff,2048,url);
+
+    return Py_BuildValue("y", buff);
+};
 
 
 
-static PyMethodDef http_methods[] = {
+
+static PyMethodDef p2ptest_methods[] = {
     {"requestHttpGET", reqGET, METH_VARARGS},
+    {"requestHttpsGET", reqGETs, METH_VARARGS},
     {NULL},
 };
 
-static struct PyModuleDef http = {
+static struct PyModuleDef p2ptest = {
     PyModuleDef_HEAD_INIT,
     "http",
     "Python3 C API Module for http and https request",
     -1,
-    http_methods
+    p2ptest_methods
 };
 
 //module creator
-PyMODINIT_FUNC PyInit_hello(void)
+PyMODINIT_FUNC PyInit_p2ptest(void)
 {
-    return PyModule_Create(&http);
+    return PyModule_Create(&p2ptest);
 }
